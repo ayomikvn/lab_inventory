be2f65cf71250f006aa91ae96394c1cac8b43f87
Ayomikun Orogun <AOrogun@sonicwall.com>
2018-04-19 16:59:08 -0700

    Request and Return device are fully functional.

--- podrequest/urls.py
+++ podrequest/urls.py
@@ -5,8 +5,8 @@ from podrequest import views
 app_name = 'podrequest'
 
 urlpatterns = [
-    url(r'^$', views.DeviceListView.as_view(), name='index'),
+    url(r'^$', views.DeviceListView.as_view(), name='device_list'),
+    url(r'^requestDevice/', views.requestDevice, name='requestDevice'),
     url(r'^history/', views.HistoryListView.as_view(), name='requesthistory'),
-    url(r'^requestdevice/', views.requestdevice, name='requestdevice'),
     url(r'^returnDevice/', views.returnDevice, name='returnDevice'),
 ]
