57183fc4977c295934a69a52a8ed8582b6473a79
Ay0mikun <AOrogun@sonicwall.com>
2018-06-21 15:46:55 -0700

    Changed name to Pod request Tracker. Edited base.html for auth_app to use bootstrap.bundle.min.js

--- podrequest/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from podrequest.models import Device, RequestHistory
-# Register your models here.
-admin.site.register(Device)
-admin.site.register(RequestHistory)
--- podrequest/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PodrequestConfig(AppConfig):
-    name = 'podrequest'
